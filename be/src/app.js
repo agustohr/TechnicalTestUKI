fa62b139da75bdacd4442e4e6a5fa26a50b42df1
Agusto Hawlai R <Agusto.Hawlai@bni.co.id>
2025-12-17 23:38:14 +0700

    Init repo and add backend

---
+++ be/src/app.js
@@ -0,0 +1,33 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const corsOrigin = process.env.CORS_ORIGIN || "http://localhost:4200";
+app.use(cors({ origin: corsOrigin  }));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get("/test", (_req, res) => res.json({ ok: 'test run ok' }));
+
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+async function start() {
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("MongoDB connected:", MONGO_URI);
+
+    app.listen(PORT, () => {
+      console.log(`API running on http://localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to start server:", err);
+    process.exit(1);
+  }
+}
+
+start();
